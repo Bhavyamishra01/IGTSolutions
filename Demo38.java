3c50eea46430d9985986ed77819083f0f911ec6e
Bhavya Mishra <114763770+Bhavyamishra01@users.noreply.github.com>
2023-06-27 14:16:28 +0530

    Add files via upload

---
+++ Demo38.java
@@ -0,0 +1,60 @@
+class A {
+
+    int ss = 0;
+
+    int mm = 0;
+
+    int hh = 0;
+
+
+
+
+    void abc() {
+
+        try {
+
+            for (int i = 0; i <= 10000; i++) {
+
+                System.out.print("\r" + hh + " :" + mm + ":" + i);
+
+                if (i == 60) {
+
+                    mm++;
+
+                    i = 0;
+
+                }
+
+                Thread.sleep(1000);
+
+            }
+
+        } catch (InterruptedException e) {
+
+
+
+
+        }
+
+    }
+
+}
+
+
+
+
+public class Demo38 {
+
+    public static void main(String[] args) {
+
+        A a = new A();
+
+        a.abc();
+
+    }
+
+
+
+
+}
+
